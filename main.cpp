6c622c3ec8c511dfa2825638927366da1a142c65
florianboisdron <fboisdro@etudiant.univ-lr.fr>
2024-09-13 15:18:06 +0200

    Une fenetre s'affiche

--- main.cpp
+++ main.cpp
@@ -2,12 +2,37 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "Erreur SDL: " << SDL_GetError() << std::endl;
-        return 1;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());
+        return EXIT_FAILURE;
     }
+    SDL_Window* pWindow{ nullptr };
+    SDL_Renderer* pRenderer{ nullptr };
+    if (SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_SHOWN, &pWindow, &pRenderer) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    SDL_Event events;
+    bool isOpen{ true };
+    while (isOpen)
+    {
+        while (SDL_PollEvent(&events))
+        {
+            switch (events.type)
+            {
+                case SDL_QUIT:
+                    isOpen = false;
+                break;
+            }
+        }
+    }
+    SDL_DestroyRenderer(pRenderer);SDL_DestroyWindow(pWindow);
     SDL_Quit();
-    std::cout << "SDL initialisé avec succès!" << std::endl;
-    return 0;
+    return EXIT_SUCCESS;
+
 }
 
