1446a4ec9a26b4b51be82441b17b9efbddeb9aa7
dansunavioon <115085867+dansunavioon@users.noreply.github.com>
2024-11-05 11:20:35 +0100

    timer personnage

--- Fenetre/Fenetre.cpp
+++ Fenetre/Fenetre.cpp
@@ -53,8 +53,12 @@ int Fenetre::display(){
     // Boucle infinie qui empeche la destruction de la fenetre tant que la fenetre n'est pas fermée
     bool isOpen = true;
 
+    // Timer pour l'animation
+    Uint32 animationTimer = 0; // Temps d'animation
+    const Uint32 animationDelay = 100; // Délai en millisecondes pour la mise à jour de l'animation
+
     while (isOpen) {
-        // Gérer les événements utilisateur (déplacements du personnage et fermeture de la fenêtre)
+        // Gérer les événements utilisateur
         while (SDL_PollEvent(&events)) {
             switch (events.type) {
                 case SDL_QUIT:
@@ -66,10 +70,18 @@ int Fenetre::display(){
             }
         }
 
+        // Temps actuel
+        Uint32 currentTime = SDL_GetTicks();
+
+        // Vérifie si le temps écoulé depuis la dernière mise à jour de l'animation est supérieur au délai
+        if (currentTime - animationTimer >= animationDelay) {
+            plongeur.update(); // Mettre à jour l'animation
+            animationTimer = currentTime; // Réinitialiser le timer
+        }
+
         // Mettre à jour l'état du personnage (animation, position, etc.)
         // NB : Penser à faire en sorte que la boucle de calcul ait un petit wait (à limiter, essayer de le faire pour que chaque frame de calcul dure le même temps)
         // NB : pour les animations : là ça doit être obligatoire de limiter les updates mais bien distinguer du calcul (vitesse des animations potentiellement très différentes des calculs)
-        plongeur.update();
 
         // Mettre à jour la caméra en fonction de la position du personnage
         SDL_Rect personnageRect = {plongeur.getX(), plongeur.getY(), TAILLE_PLONGEUR, TAILLE_PLONGEUR};
