b3651419499e5df7232c532554e4037f51e7a572
Kylian <kylian.de.lacour@gmail.com>
2024-12-04 16:17:11 +0100

    Merge

--- Fenetre/Fenetre.h
+++ Fenetre/Fenetre.h
@@ -3,6 +3,9 @@
 
 #include <SDL.h>
 #include <string>
+#include <vector>
+#include "../Personage/Personnage.h"
+#include "../Poisson/Poisson.h"
 
 class Fenetre
 {
@@ -16,10 +19,17 @@ public:
     void effacer();
     void afficher();
 
+    void updateCamera(const SDL_Rect& personnageRect);
+    int display();
+
 private:
     SDL_Window* window;
     SDL_Renderer* renderer;
+    SDL_Rect camera; // Caméra pour suivre le personnage
     bool valide;
+
+    int height;
+    int width;
 };
 
 #endif // FENETRE_H
