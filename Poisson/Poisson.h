f231e44a7dc611a91de5fa67e44590301c2b5b7f
dansunavioon <115085867+dansunavioon@users.noreply.github.com>
2024-12-09 15:24:21 +0100

    poisson 1

--- Poisson/Poisson.h
+++ Poisson/Poisson.h
@@ -4,24 +4,38 @@
 #include <SDL.h>
 #include <vector>
 
+// Dimensions de la carte
+extern const int MAP_WIDTH;
+extern const int MAP_HEIGHT;
+
+// Dimensions de la caméra
+extern const int CAMERA_WIDTH;
+extern const int CAMERA_HEIGHT;
+
+
 class Poisson
 {
 public:
-    Poisson(float x, float y);
+    Poisson(SDL_Renderer* renderer, float x, float y, bool independent);
+    ~Poisson();
+
     void update(const std::vector<Poisson>& poissons);
-    void draw(SDL_Renderer* renderer) const;
+    void draw(SDL_Renderer* renderer, const SDL_Point& cameraPosition) const;
 
 private:
-    SDL_Point position;
-    SDL_Point velocity;
-    bool independent;
-    int groupId;  // Ajout de l'ID de groupe pour chaque poisson
-
     void applyBehaviors(const std::vector<Poisson>& poissons);
     SDL_Point cohesionBehavior(const std::vector<Poisson>& poissons);
     SDL_Point separationBehavior(const std::vector<Poisson>& poissons);
+    float angle; // Angle actuel du poisson pour la rotation
 
-    static const int MAX_GROUPS = 5; // Définir le nombre de groupes max (ajustez selon vos besoins)
+    SDL_Point position;
+    SDL_Point velocity;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    bool independent;
+    int groupId;
+
+    static constexpr int MAX_GROUPS = 15;
 };
 
 #endif // POISSON_H
