9ef124521799b37c98803c28c9397c511454589e
dansunavioon <115085867+dansunavioon@users.noreply.github.com>
2024-12-18 14:00:23 +0100

    debut menu

--- Fenetre/Fenetre.cpp
+++ Fenetre/Fenetre.cpp
@@ -15,6 +15,12 @@ Mix_Music* backgroundMusic = nullptr;
  * @param width
  */
 
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// -------------- Constructeur
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
 Fenetre::Fenetre(SDL_Window* window, SDL_Renderer* renderer, int height, int width) : window(window), renderer(renderer), height(height), width(width) {
 
     // Initialisation de la caméra
@@ -37,6 +43,12 @@ Fenetre::Fenetre(SDL_Window* window, SDL_Renderer* renderer, int height, int wid
     Mix_PlayMusic(backgroundMusic, -1);
 }
 
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// -------------- Camera
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
 void Fenetre::updateCamera(const SDL_Rect& personnageRect) {
     // Centrer la caméra sur le personnage
     camera.x = personnageRect.x + (TAILLE_PLONGEUR / 2) - (SCREEN_WIDTH / 2);
@@ -49,13 +61,134 @@ void Fenetre::updateCamera(const SDL_Rect& personnageRect) {
     if (camera.y > MAP_HEIGHT - camera.h) camera.y = MAP_HEIGHT - camera.h;
 }
 
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// -------------- Bouton Paramètre
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+void Fenetre::drawButton(SDL_Renderer* renderer, const char* text, TTF_Font* font, SDL_Color textColor, SDL_Color normalColor, SDL_Color hoverColor) {
+    // Vérifie si la souris est sur le bouton
+    bool isHovered = isMouseInsideButton(BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    // Choisir la couleur selon l'état du bouton
+    SDL_Color buttonColor = isHovered ? hoverColor : normalColor;
+
+    // Dessiner le bouton
+    SDL_SetRenderDrawColor(renderer, buttonColor.r, buttonColor.g, buttonColor.b, 255);
+    SDL_Rect buttonRect = { BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT };
+    SDL_RenderFillRect(renderer, &buttonRect);
+
+    // Dessiner les contours du bouton (facultatif)
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderDrawRect(renderer, &buttonRect);
+
+    // Dessiner le texte du bouton
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text, textColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    // Calculer la position pour centrer le texte
+    int textWidth = textSurface->w;
+    int textHeight = textSurface->h;
+    SDL_Rect textRect = {
+            BUTTON_X + (BUTTON_WIDTH - textWidth) / 2,
+            BUTTON_Y + (BUTTON_HEIGHT - textHeight) / 2,
+            textWidth,
+            textHeight
+    };
+
+    SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
+
+    // Libérer les ressources
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
+}
+
+bool Fenetre::isMouseInsideButton(int x, int y, int width, int height) {
+    int mouseX, mouseY;
+    SDL_GetMouseState(&mouseX, &mouseY);
+    return (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height);
+}
+
+void Fenetre::openVolumeSettings() {
+    SDL_Window* volumeWindow = SDL_CreateWindow("Volume", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 200, SDL_WINDOW_SHOWN);
+    SDL_Renderer* volumeRenderer = SDL_CreateRenderer(volumeWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    TTF_Font* font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16); // Chargez une police (assurez-vous d'installer SDL_ttf)
+    if (!font) {
+        SDL_Log("Erreur : Impossible de charger la police : %s", TTF_GetError());
+    }
+
+    bool isVolumeOpen = true;
+    SDL_Event volumeEvent;
+
+    // Définir les dimensions et position du bouton "OK"
+    const int okButtonWidth = 80;
+    const int okButtonHeight = 40;
+    const int okButtonX = (400 - okButtonWidth) / 2; // Centré horizontalement
+    const int okButtonY = 150;                       // Position verticale vers le bas
+
+    // Couleurs pour le texte et le bouton
+    SDL_Color buttonTextColor = {255, 255, 255}; // Texte blanc
+    SDL_Color buttonNormalColor = {100, 100, 100}; // Bouton gris clair
+    SDL_Color buttonHoverColor = {150, 150, 150};  // Bouton gris survolé
+
+    while (isVolumeOpen) {
+        while (SDL_PollEvent(&volumeEvent)) {
+            switch (volumeEvent.type) {
+                case SDL_QUIT:
+                    isVolumeOpen = false;  // Fermer la fenêtre
+                    break;
+
+                case SDL_MOUSEBUTTONDOWN:
+                    // Vérifier si le bouton "OK" a été cliqué
+                    if (isMouseInsideButton(okButtonX, okButtonY, okButtonWidth, okButtonHeight)) {
+                        isVolumeOpen = false; // Fermer la fenêtre
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // Rafraîchir la sous-fenêtre
+        SDL_SetRenderDrawColor(volumeRenderer, 0, 0, 0, 255); // Fond noir
+        SDL_RenderClear(volumeRenderer);
+
+        // Afficher le bouton "OK"
+        drawButton(volumeRenderer, "OK", font, buttonTextColor, buttonNormalColor, buttonHoverColor);
+
+        SDL_RenderPresent(volumeRenderer);
+    }
+
+    // Nettoyer
+    SDL_DestroyRenderer(volumeRenderer);
+    SDL_DestroyWindow(volumeWindow);
+}
+
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// -------------- Affichage
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
 int Fenetre::display(){
 
     // Initialisation
     SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
 
     this->window = SDL_CreateWindow("Poisson", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, this->width, this->height, SDL_WINDOW_SHOWN);
     this->renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    // Création d'un bouton
+    TTF_Font* font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16); // Chargez une police (assurez-vous d'installer SDL_ttf)
+    if (!font) {
+        SDL_Log("Erreur : Impossible de charger la police : %s", TTF_GetError());
+    }
+
 
     // Créer un objet Personnage avec le renderer
     Personnage plongeur(renderer);
@@ -75,6 +208,14 @@ int Fenetre::display(){
             switch (events.type) {
                 case SDL_QUIT:
                     isOpen = false;  // Quitter l'application
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    int mouseX, mouseY;
+                    SDL_GetMouseState(&mouseX, &mouseY);
+                    if (isMouseInsideButton(BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT)) {
+                        // Ouvrir la sous-fenêtre de gestion du volume
+                        openVolumeSettings();
+                    }
                     break;
                 default:
                     plongeur.handleInput(events);  // Gérer les entrées pour déplacer le personnage
@@ -127,6 +268,14 @@ int Fenetre::display(){
         // Rendre le personnage à sa nouvelle position
         plongeur.render();
 
+        SDL_Color normalColor = {128, 128, 128, 255}; // Gris
+        SDL_Color hoverColor = {160, 160, 160, 255};  // Gris clair
+        SDL_Color textColor = {255, 255, 255, 255};   // Blanc
+
+        // Pendant le rendu
+        drawButton(renderer, "Parametre", font, textColor, normalColor, hoverColor);
+
+
         // Afficher le rendu à l'écran
         SDL_RenderPresent(renderer);
     }
@@ -134,6 +283,7 @@ int Fenetre::display(){
     // Quitter proprement
     Mix_FreeMusic(backgroundMusic);
     Mix_CloseAudio();
+    TTF_Quit();
     delete map;
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
--- Fenetre/Fenetre.h
+++ Fenetre/Fenetre.h
@@ -2,6 +2,8 @@
 #define FENETRE_H
 
 #include <SDL.h>
+#include <SDL_ttf.h>
+#include <string>
 #include "../Map/Map.h"
 
 class Fenetre {
@@ -17,7 +19,9 @@ class Fenetre {
         Fenetre(SDL_Window* window, SDL_Renderer* renderer, int height, int width);
         int display();
         void updateCamera(const SDL_Rect& personnageRect); // Mise à jour de la camera
-        void openSettingsWindow();
+        void openVolumeSettings();
+        bool isMouseInsideButton(int x, int y, int width, int height);
+        void drawButton(SDL_Renderer* renderer, const char* text, TTF_Font* font, SDL_Color textColor, SDL_Color normalColor, SDL_Color hoverColor);
 
 };
 
